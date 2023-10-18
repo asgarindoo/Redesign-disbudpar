2eeee2d67f05bec0ceb050cae5ca2cf0df0b7c54
Asgarindo Dwiki Ibrahim adji <asgarindo9@gmail.com>
2023-10-18 12:57:50 +0700

    Update Navbar Beranda

---
+++ Redesign-disbudpar/wwwroot/js/scrollNav.js
@@ -0,0 +1,10 @@
+﻿window.addEventListener('scroll', function () {
+    var navbar = document.querySelector('.navbar-beranda');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition >= 590) {
+        navbar.classList.add('navbar-fixed');
+    } else {
+        navbar.classList.remove('navbar-fixed');
+    }
+});
