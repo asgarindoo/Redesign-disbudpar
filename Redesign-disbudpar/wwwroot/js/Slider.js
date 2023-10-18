f7ec261ffe2a42da8ca807417c8fa829d02f8b51
Asgarindo Dwiki Ibrahim adji <asgarindo9@gmail.com>
2023-10-18 15:21:12 +0700

    Upsate Slider

---
+++ Redesign-disbudpar/wwwroot/js/Slider.js
@@ -0,0 +1,26 @@
+﻿const slides = document.querySelectorAll('.slides');
+const dots = document.querySelectorAll('.dot');
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+    slides.forEach((slide) => (slide.style.display = 'none'));
+    dots.forEach((dot) => dot.classList.remove('active'));
+    slides[slideIndex].style.display = 'block';
+    dots[slideIndex].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        showSlide(index);
+        currentSlide = index;
+    });
+});
+
+showSlide(currentSlide);
+
+setInterval(nextSlide, 5000);
