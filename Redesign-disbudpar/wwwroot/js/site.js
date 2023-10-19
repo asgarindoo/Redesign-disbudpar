d9357da35b9b9e6680c9980164fe9dd2153c2201
Asgarindo Dwiki Ibrahim adji <asgarindo9@gmail.com>
2023-10-19 12:52:30 +0700

    update

--- Redesign-disbudpar/wwwroot/js/site.js
+++ Redesign-disbudpar/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+    const apiKey = 'cfab2e47d70a22f05b460dc15e059bf0';
+    const city = 'Brebes';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+    .then((response) => response.json())
+        .then((data) => {
+            const location = 'Brebes';
+
+            document.getElementById('location').textContent = location;
+            document.getElementById('temperature').textContent = data.main.temp;
+            document.getElementById('condition').textContent = data.weather[0].description;
+    })
+    .catch((error) => {
+        console.error('Terjadi kesalahan:', error);
+    });
